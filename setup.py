d11badb66a760e04a5dd25ba9fa510d323c34860
pratikyadav <pratik@mapbox.com>
2017-06-21 14:11:41 -0700

    readme update

--- setup.py
+++ setup.py
@@ -2,15 +2,16 @@ from codecs import open as codecs_open
 from setuptools import setup, find_packages
 
 
+
 setup(name='testpy',
       version='0.0.1',
       description=u"Skeleton of a Python package",
-      long_description=long_description,
+      long_description='long_description',
       classifiers=[],
       keywords='',
       author=u"Sean Gillies",
       author_email='sean@mapbox.com',
-      url='https://github.com/mapbox/testpy',
+      url='https://github.com/pratikyadav/testpy',
       license='MIT',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
